02b5b798ec2bd9f646c8f1b8791bbe223c2bd692
Kristofor Carle <kristofor.carle@syncadd.com>
2013-05-13 13:17:24 -0400

    adding user provided layer types

--- src/core/floorplan.js
+++ src/core/floorplan.js
@@ -1,20 +1,28 @@
 
 
-ofp.FloorPlan = function (container) {
+ofp.FloorPlan = function (container, layerTypes) {
     'use strict';
 
     this.svg = d3.select(container).select("svg");
 
-
-    this.spaces = new ofp.Layer(ofp.LayerType.Space, this);
-    this.layers.push(this.spaces);
-    this.columns = new ofp.Layer(ofp.LayerType.Column, this);
-    this.layers.push(this.columns);
-    this.constructions = new ofp.Layer(ofp.LayerType.Construction, this);
-    this.layers.push(this.constructions);
-    this.dimensionAnnotations = new ofp.Layer(ofp.LayerType.DimensionAnnotations, this);
-    this.layers.push(this.dimensionAnnotations);
-
+    if (layerTypes) { //use user provided layer types
+        var x;
+        for (x in layerTypes) {
+            if (layerTypes.hasOwnProperty(x)) {
+                this[x] = layerTypes[x];
+                this.layers.push(this[x]);
+            }
+        }
+    } else { //use pre-configured layer types
+        this.spaces = new ofp.Layer(ofp.LayerType.Space, this);
+        this.layers.push(this.spaces);
+        this.columns = new ofp.Layer(ofp.LayerType.Column, this);
+        this.layers.push(this.columns);
+        this.constructions = new ofp.Layer(ofp.LayerType.Construction, this);
+        this.layers.push(this.constructions);
+        this.dimensionAnnotations = new ofp.Layer(ofp.LayerType.DimensionAnnotations, this);
+        this.layers.push(this.dimensionAnnotations);
+    }
     //private
     var parent = container;
 
